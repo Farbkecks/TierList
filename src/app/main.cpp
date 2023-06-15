23abdfc808f5d3ec1e2944c9d92723b3ba6a79f6
Fabian A <>
2023-06-15 23:08:54 +0200

    rename MainWindow

--- src/app/main.cpp
+++ src/app/main.cpp
@@ -1,13 +1,13 @@
 #include <QApplication>
 #include <QPushButton>
-#include "mainwindow.h"
+#include "MainWindow.h"
 #include <iostream>
 
 int main(int argc, char *argv[]) {
     std::cout << "Test";
     QApplication a(argc, argv);
     MainWindow w;
-    
+
     w.setWindowState(Qt::WindowMaximized);
     w.show();
     return QApplication::exec();
